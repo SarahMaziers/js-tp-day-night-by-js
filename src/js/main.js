4ef14677098e675118021b10c12e276a427dc7f9
Sarah-Maziers <sarah.maziers@student.hepl.be>
2020-12-03 12:10:59 +0100

    resolution

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,23 @@
-document.documentElement.classList.add('js-enabled');
-const button = document.querySelector('.tumbler__wrapper');
-const e_body=document.body;
-const posts=document.querySelectorAll('.post');
-const e_tumblr=document.querySelector('.tumbler');
-button.addEventListener('click',function () {
-    e_body.classList.toggle('body--night-mode');
-    e_tumblr.classList.toggle('tumbler--night-mode');
-    for(const post of posts){
-        post.classList.toggle('post--night-mode');
+
+
+const o_night={
+    init(){
+        document.documentElement.classList.add('js-enabled');
+        const button = document.querySelector('.tumbler__wrapper');
+        const e_body=document.body;
+        const posts=document.querySelectorAll('.post');
+        const e_tumblr=document.querySelector('.tumbler');
+        button.addEventListener('click',function () {
+            e_body.classList.toggle('body--night-mode');
+            e_tumblr.classList.toggle('tumbler--night-mode');
+            for(const post of posts){
+                post.classList.toggle('post--night-mode');
+            }
+        })
     }
-})
+
+}
+o_night.init();
 
 
 
